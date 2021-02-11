e1c64752b04f260d8a76c00d639f16b498764b68
Alexandre Thery <alexandrethery1202@gmail.com>
2021-02-11 16:53:26 +0100

    Site updated at 2021-02-11 15:53:26 UTC

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "JDR",
+        "excerpt":"Le Temps d’une Pause à Westcliff Début du scénar : France ou ailleurs, bord de mer, les années 2020. Personnages en road-trip. Un soir aux abords d’un village côtier, leur voiture vient s’écraser contre un arbre tombé à terre, abattu par la foudre il y a peu. Sur cet arbre...","categories": ["JDR"],
+        "tags": [],
+        "url": "/jdr/JDR-1/",
+        "teaser": null
+      },{
+        "title": "Test",
+        "excerpt":"   Blog Name                   TITLE HEADING        Title description, Dec 7, 2017        Image       Some text..                  TITLE HEADING        Title description, Sep 2, 2017        Image       Some text..                        About Me        Image       Some text about me in culpa qui officia deserunt mollit anim..                  Popular Post        Image       Image       Image                 Follow Me        Some text..               Footer  ","categories": [],
+        "tags": [],
+        "url": "/test/",
+        "teaser": null
+      }]
